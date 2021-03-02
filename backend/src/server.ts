6ab5a7cbabbab028206f5985300545b6f6dca11a
Yan Garcia <yangarciavaz@gmail.com>
2021-03-01 21:09:30 -0300

    error handler created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json()); // pro express entender o json
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333); //localhost:3333 e esse número da porta pode ser trocado
